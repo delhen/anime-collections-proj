22f4b637bb6118a597c73a87839cdb6913cc91f1
delhen <hendardidel@gmail.com>
2022-07-19 17:33:18 +0700

    Adjustment on app.js, app.css, and index.css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import { Routes, Route, Link } from "react-router-dom";
 import './App.css';
 import Container from "./components/Container/Container";
+import Header from "./components/Header/Header";
 import Home from "./pages/Home/Home";
+import UserCollection from "./pages/UserCollection/UserCollection";
 
 function App() {
   return (
-    <Container>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Container>
+    <div className="App">
+      <Container>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/collections" element={<UserCollection />} />
+        </Routes>
+      </Container>
+    </div>
   );
 }
 
