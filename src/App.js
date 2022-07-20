770a05df051f962ef6748835696a66d3ab6628ca
delhen <hendardidel@gmail.com>
2022-07-20 23:34:11 +0700

    Add provider for created context

--- src/App.js
+++ src/App.js
@@ -6,18 +6,24 @@ import AnimeDetail from "./pages/AnimeDetail/AnimeDetail";
 import AnimeList from "./pages/AnimeList/AnimeList";
 import UserCollection from "./pages/UserCollection/UserCollection";
 import CollectionDetail from "./pages/CollectionDetail/CollectionDetail";
+import { AnimeWithCollectionContext, CollectionContext } from "./utils/Context";
+import { initCollectionListObj } from "./utils/CommonHelper";
 
 function App() {
   return (
     <div className="App">
       <Container>
         <Header />
-        <Routes>
-          <Route path="/anime/:id" element={<AnimeDetail />} />
-          <Route path="/collections" element={<UserCollection />} />
-          <Route path="/collection/:id" element={<CollectionDetail />} />
-          <Route path="/" element={<AnimeList />} />
-        </Routes>
+        <CollectionContext.Provider value={initCollectionListObj('collection-list')}>
+            <AnimeWithCollectionContext.Provider value={initCollectionListObj('anime-with-collections')}>
+              <Routes>
+                <Route path="/anime/:id" element={<AnimeDetail />} />
+                <Route path="/collections" element={<UserCollection />} />
+                <Route path="/collection/:id" element={<CollectionDetail />} />
+                <Route path="/" element={<AnimeList />} />
+              </Routes>
+            </AnimeWithCollectionContext.Provider>
+        </CollectionContext.Provider>
       </Container>
     </div>
   );
