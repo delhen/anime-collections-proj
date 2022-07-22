c53967f259b33181d4955eebc6d159e3cad97e8b
delhen <hendardidel@gmail.com>
2022-07-22 13:32:53 +0700

    Add navigation to collection detail

--- src/components/CollectionList/CollectionList.js
+++ src/components/CollectionList/CollectionList.js
@@ -1,22 +1,27 @@
 /** @jsxImportSource @emotion/react */
+import { useNavigate } from "react-router-dom";
 import Collection from "./Collection/Collection";
 import { collectionListStyle } from "./CollectionListStyle"
 
 const CollectionList = (props) => {
+  const navigate = useNavigate();
   let component = (
     <div>
       <h4>No collection with this anime</h4>
     </div>
   )
 
-  // console.log('[CollectionList] props.collections: ', props.collections)
+  const goToCollectionDetail = (id) => {
+    navigate(`/collection/${id}`);
+    window.scrollTo(0, 0);
+  }
 
   if(Object.keys(props.collections).length > 0){
     component = (
       <div css={collectionListStyle}>
         {
           props.collections.map((collection) => {
-            return <Collection name={collection.name} key={collection.id} />
+            return <Collection name={collection.name} key={collection.id} click={() => goToCollectionDetail(collection.id)} />
           })
         }
       </div>
