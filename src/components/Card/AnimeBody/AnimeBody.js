ec97363649e54db7fb771292d97f004a0cf98166
delhen <hendardidel@gmail.com>
2022-07-22 08:35:08 +0700

    Add remove from collection button

--- src/components/Card/AnimeBody/AnimeBody.js
+++ src/components/Card/AnimeBody/AnimeBody.js
@@ -1,16 +1,41 @@
 /** @jsxImportSource @emotion/react */
+import { useContext } from 'react';
 import { getRatingBgColor } from '../../../utils/CommonHelper';
+import { CollectionContext } from '../../../utils/Context';
 import { ratingStyle } from './AnimeBodyStyle'
 
 const AnimeBody = (props) => {
+  const collectionContext = useContext(CollectionContext);
   const rating = parseInt(props.rating);
   let ratingBgColor = getRatingBgColor(rating);
+  // alert(props['animeId'])
+  const processRemove = (e) => {
+    e.stopPropagation();
+    if(window.confirm("Remove this from collection " + props.fromCollection + "?")){
+      const animesReplicate = collectionContext.collections[props.fromCollection].animes;
+      delete animesReplicate[props.animeId];
+      // console.log(animesReplicate)
+      // delete collectionContext[props.fromCollection].animes[props.animeId];
+      // console.log(collectionContext)
+      // localStorage.setItem("collection-list", JSON.stringify(collectionContext));
+      collectionContext.setCollections({
+        ...collectionContext.collections,
+        [props.fromCollection]: {
+          ...collectionContext.collections[props.fromCollection],
+          animes: animesReplicate,
+        }
+      })
+
+      localStorage.setItem("collection-list", JSON.stringify(collectionContext.collections));
+    }
+  }
 
   return(
-    <div>
+    <div css={{padding: '8px'}}>
       <p><span css={{...ratingStyle, backgroundColor: ratingBgColor}}>{props.rating}</span></p>
       <p><b>{props.title != null ? props.title : props.title_native}</b></p>
       <p><small><i>{props.title_native != null != "" ? props.title_native : props.title}</i></small></p>
+      {props.showRemove && <button onClick={e => processRemove(e)}>Remove</button>}
     </div>
   )
 }
