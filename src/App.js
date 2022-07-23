44b85b6c8556ecf38adc525c99fd04eefdd17ff6
delhen <hendardidel@gmail.com>
2022-07-23 20:01:19 +0700

    Add modal context

--- src/App.js
+++ src/App.js
@@ -6,27 +6,30 @@ import AnimeDetail from "./pages/AnimeDetail/AnimeDetail";
 import AnimeList from "./pages/AnimeList/AnimeList";
 import UserCollection from "./pages/UserCollection/UserCollection";
 import CollectionDetail from "./pages/CollectionDetail/CollectionDetail";
-import { AnimeWithCollectionContext, CollectionContext } from "./utils/Context";
+import { AnimeWithCollectionContext, CollectionContext, ModalContext } from "./utils/Context";
 import { initCollectionListObj } from "./utils/CommonHelper";
 import { useState } from "react";
 
 function App() {
   const [collections, setCollections] = useState(initCollectionListObj('collection-list'))
   const [animeCollections, setAnimeCollections] = useState(initCollectionListObj('anime-with-collections'))
+  const [showModal, setShowModal] = useState(false);
   return (
     <div className="App">
       <Container>
         <Header />
-        <CollectionContext.Provider value={{collections, setCollections}}>
-            <AnimeWithCollectionContext.Provider value={{animeCollections, setAnimeCollections}}>
-              <Routes>
-                <Route path="/anime/:id" element={<AnimeDetail />} />
-                <Route path="/collections" element={<UserCollection />} />
-                <Route path="/collection/:id" element={<CollectionDetail />} />
-                <Route path="/" element={<AnimeList />} />
-              </Routes>
-            </AnimeWithCollectionContext.Provider>
-        </CollectionContext.Provider>
+        <ModalContext.Provider value={{showModal, setShowModal}}>
+          <CollectionContext.Provider value={{collections, setCollections}}>
+              <AnimeWithCollectionContext.Provider value={{animeCollections, setAnimeCollections}}>
+                <Routes>
+                  <Route path="/anime/:id" element={<AnimeDetail />} />
+                  <Route path="/collections" element={<UserCollection />} />
+                  <Route path="/collection/:id" element={<CollectionDetail />} />
+                  <Route path="/" element={<AnimeList />} />
+                </Routes>
+              </AnimeWithCollectionContext.Provider>
+          </CollectionContext.Provider>
+        </ModalContext.Provider>
       </Container>
     </div>
   );
--- src/utils/Context.js
+++ src/utils/Context.js
@@ -1,3 +1,4 @@
 import React from 'react';
 export const CollectionContext = React.createContext();
 export const AnimeWithCollectionContext = React.createContext();
+export const ModalContext = React.createContext(false);
