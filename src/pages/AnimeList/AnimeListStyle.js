22c84206820a0a920b4dab8447af4d0ae19a8348
delhen <hendardidel@gmail.com>
2022-07-23 20:02:22 +0700

    Refactoring anime list style

--- src/pages/AnimeList/AnimeListStyle.js
+++ src/pages/AnimeList/AnimeListStyle.js
@@ -4,18 +4,5 @@ export const animeListContainerStyle = {
   justifyContent: 'center',
   alignItems: "center",
   width: "100%",
-}
-
-export const animeListStyle = {
-  display: 'grid',
-  gap: '8px',
-  padding: '16px',
-  gridTemplateColumns: 'repeat(2, 1fr)',
-  gridAutoRow: '1fr',
-  '@media (min-width: 600px)': {
-    gridTemplateColumns: 'repeat(3, 1fr)',
-  },
-  '@media (min-width: 900px)': {
-    gridTemplateColumns: 'repeat(4, 1fr)',
-  }
+  marginTop: "24px"
 }
