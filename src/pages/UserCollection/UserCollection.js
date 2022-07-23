73779cabb2d41a40faf7c978b78ff3344912c506
delhen <hendardidel@gmail.com>
2022-07-23 11:05:24 +0700

    Add edit collection name on collection list

--- src/pages/UserCollection/UserCollection.js
+++ src/pages/UserCollection/UserCollection.js
@@ -4,8 +4,8 @@ import Pagination from "../../components/Pagination/Pagination";
 import CollectionBody from "../../components/Card/CollectionBody/CollectionBody";
 import { addCollectionBtnStyle, addNewCollectionBtnStyle, containerLayout, gridLayout, inputTextStyle } from "./UserCollectionStyle";
 import { useContext, useRef, useState } from "react";
-import { CollectionContext } from "../../utils/Context";
-import { createRandomId, getAllCollection, validateExistingName, validateSpecialChars } from "../../utils/CommonHelper";
+import { AnimeWithCollectionContext, CollectionContext } from "../../utils/Context";
+import { createRandomId, getAllCollection, getAnimesFromCollection, getKeyFromObject, validateExistingName, validateSpecialChars } from "../../utils/CommonHelper";
 import Modal from "../../components/Modal/Modal";
 import noCover from './no_image_cover.jpg';
 import Button from "../../components/Button/Button";
@@ -13,8 +13,42 @@ import Button from "../../components/Button/Button";
 function UserCollection() {
   const collectionContext = useContext(CollectionContext);
   const collections = getAllCollection(collectionContext.collections);
+  const animeWithCollectionContext = useContext(AnimeWithCollectionContext);
   const [show, setShow] = useState(false);
+  const [formType, setFormType] = useState("add");
+  const [collectionId, setCollectionId] = useState();
+  const [collectionName, setCollectionName] = useState();
   const collectionNameRef = useRef(null);
+
+  const editCollectionName = (collectionId) => {
+    if(collectionNameRef.current.value === null || collectionNameRef.current.value === "") alert("Collection name cannot be empty!");
+    else if(validateSpecialChars(collectionNameRef.current.value)) alert("Collection name has special characters!");
+    else if(!validateExistingName(collectionNameRef.current.value, getAllCollection(collectionContext.collections))) alert("Collection name already exist!")
+    else{
+      const newName = collectionNameRef.current.value;
+
+      collectionContext.collections[collectionId] = {
+        ...collectionContext.collections[collectionId],
+        name: newName
+      }
+
+      const animeCollecitons = animeWithCollectionContext.animeCollections;
+      const keyAnimeCollections = getKeyFromObject(animeCollecitons);
+      keyAnimeCollections.forEach(animeId => {
+        if(animeCollecitons[animeId].collections[collectionId] !== undefined){
+          animeCollecitons[animeId].collections[collectionId] = {
+            ...animeCollecitons[animeId].collections[collectionId],
+            name: newName,
+          }
+        }
+      })
+
+      localStorage.setItem("collection-list", JSON.stringify(collectionContext.collections));
+      localStorage.setItem("anime-with-collections", JSON.stringify(animeWithCollectionContext.animeCollections));
+      alert("Collection successfully edited!");
+      setShow(false);
+    }
+  }
 
   const addNewCollection = () => {
     console.log(collectionContext)
@@ -36,9 +70,37 @@ function UserCollection() {
     }
   }
 
-  return (
-    <div css={containerLayout}>
-      <Button color="white" clickAction={() => setShow(true)}>+ Add New Collection</Button>
+  let component = <h4>You have no collection recently. Lets add a new one!</h4>
+  let formAddEdit = (
+    <>
+      <div>
+        <h4>Add New Collection</h4>
+      </div>
+      <div>
+        <div>
+          <input type="text" placeholder="New collection name" css={inputTextStyle} ref={collectionNameRef} />
+          <Button color="pink" clickAction={addNewCollection}>+ New</Button>
+        </div>
+      </div>
+    </>
+  )
+  if(formType == 'edit'){
+    formAddEdit = (
+      <>
+        <div>
+          <h4>Edit Collection: {collectionName}</h4>
+        </div>
+        <div>
+          <div>
+            <input type="text" placeholder="New collection name" css={inputTextStyle} ref={collectionNameRef} />
+            <Button color="pink" clickAction={() => editCollectionName(collectionId)}>Save</Button>
+          </div>
+        </div>
+      </>
+    )
+  }
+  if(collections.length > 0){
+    component = (
       <div css={gridLayout}>
         {
           collections.map(collection => {
@@ -54,23 +116,30 @@ function UserCollection() {
           
             return (
               <Card img_url={imgUrl} type="collection" key={collection.id} id={collection.id}>
-                <CollectionBody name={collection.name} id={collection.id} />
+                <CollectionBody name={collection.name} id={collection.id} onEdit={() => {
+                  setCollectionId(collection.id);
+                  setCollectionName(collection.name);
+                  setFormType("edit");
+                  setShow(true);
+                }} />
               </Card>
             )
           })
         }
       </div>
+    )
+  }
+
+  return (
+    <div css={containerLayout}>
+      <Button color="white" clickAction={() => {
+        setShow(true);
+        setFormType('add');
+      }}>+ Add New Collection</Button>
+      {component}
       {/* <Pagination /> */}
       <Modal show={show} onClose={() => setShow(!show)}>
-        <div>
-          <h4>Add New Collection</h4>
-        </div>
-        <div>
-          <div>
-            <input type="text" placeholder="New collection name" css={inputTextStyle} ref={collectionNameRef} />
-            <Button color="pink" clickAction={addNewCollection}>+ New</Button>
-          </div>
-        </div>
+        {formAddEdit}
       </Modal>
     </div>
   );
